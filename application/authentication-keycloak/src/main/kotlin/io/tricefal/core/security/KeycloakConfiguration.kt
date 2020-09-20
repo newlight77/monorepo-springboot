2453824ecb499daf8dd16cc088a5bb4f81bceefc
Kong To <newlight77@gmail.com>
2020-09-20 11:02:11 +0200

    cleaner makefil to run core app

--- application/authentication-keycloak/src/main/kotlin/io/tricefal/core/security/KeycloakConfiguration.kt
+++ application/authentication-keycloak/src/main/kotlin/io/tricefal/core/security/KeycloakConfiguration.kt
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServletResponse
 
 
 @KeycloakConfiguration
-@Profile(value = ["prod", "dev"])
+@Profile(value = ["prod", "dev", "local"])
 class KeycloakConfiguration: KeycloakWebSecurityConfigurerAdapter() {
 
     @Autowired
