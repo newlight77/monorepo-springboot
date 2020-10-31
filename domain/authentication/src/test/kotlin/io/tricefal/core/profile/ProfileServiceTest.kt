d1d39950dc8a6a362b8cf5c4bc315ebfeb882592
Kong To <newlight77@gmail.com>
2020-10-31 23:42:32 +0100

    add unit tests for profile domain

---
+++ domain/authentication/src/test/kotlin/io/tricefal/core/profile/ProfileServiceTest.kt
@@ -0,0 +1,109 @@
+package io.tricefal.core.profile
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import java.time.Instant
+import java.util.*
+
+
+@ExtendWith(MockitoExtension::class)
+class ProfileServiceTest {
+
+    @Mock
+    lateinit var repository: IProfileAdapter
+
+    lateinit var service: IProfileService
+
+    @Test
+    fun `should create a profile successfully`() {
+        // Arrange
+        val profile = ProfileDomain("kong@gmail.com", Instant.now(), "portrait", "resume", "resume linkedin")
+        Mockito.doNothing().`when`(repository).save(profile)
+        service = ProfileService(repository)
+
+        // Act
+        service.save(profile)
+
+        // Arrange
+        Mockito.verify(repository).save(profile)
+    }
+
+    @Test
+    fun `should retrieve last profiles by username`() {
+        // Arrange
+        val username = "kong@gmail.com"
+        val profile1 = ProfileDomain(username, Instant.now(), "portrait", "resume", "resume linkedin")
+
+        val profile = Optional.of(profile1)
+
+        Mockito.`when`(repository.findByUsername(username)).thenReturn(profile)
+        service = ProfileService(repository)
+
+        // Act
+        val result = service.findByUsername(username)
+
+        // Arrange
+        Assertions.assertEquals(profile, result)
+    }
+
+    @Test
+    fun `should update the profile on portrait uploaded`() {
+        // Arrange
+        val username = "kong@gmail.com"
+        val filename = "new filename"
+        val profile1 = ProfileDomain(username, Instant.now(), "portrait", "resume", "resume linkedin")
+
+        val profile = Optional.of(profile1)
+
+        Mockito.`when`(repository.findByUsername(username)).thenReturn(profile)
+        service = ProfileService(repository)
+
+        // Act
+        val result = service.updateProfileOnPortraitUploaded(username, filename)
+
+        // Arrange
+        Assertions.assertEquals(filename, result.portraitFilename)
+    }
+
+    @Test
+    fun `should update the profile on resume uploaded`() {
+        // Arrange
+        val username = "kong@gmail.com"
+        val filename = "new filename"
+        val profile1 = ProfileDomain(username, Instant.now(), "portrait", "resume", "resume linkedin")
+
+        val profile = Optional.of(profile1)
+
+        Mockito.`when`(repository.findByUsername(username)).thenReturn(profile)
+        service = ProfileService(repository)
+
+        // Act
+        val result = service.updateProfileOnResumeUploaded(username, filename)
+
+        // Arrange
+        Assertions.assertEquals(filename, result.resumeFilename)
+    }
+
+    @Test
+    fun `should update the profile on resume linkedin uploaded`() {
+        // Arrange
+        val username = "kong@gmail.com"
+        val filename = "new filename"
+        val profile1 = ProfileDomain(username, Instant.now(), "portrait", "resume", "resume linkedin")
+
+        val profile = Optional.of(profile1)
+
+        Mockito.`when`(repository.findByUsername(username)).thenReturn(profile)
+        service = ProfileService(repository)
+
+        // Act
+        val result = service.updateProfileOnResumeLinkedinUploaded(username, filename)
+
+        // Arrange
+        Assertions.assertEquals(filename, result.resumeLinkedinFilename)
+    }
+}
