3c5938e2ef43d85082db780d607e3c745af6f81c
Kong To <newlight77@gmail.com>
2020-12-28 18:20:12 +0100

    added unit tests for cgu domain

---
+++ domain/authentication/src/test/kotlin/io/tricefal/core/cgu/CguServiceTest.kt
@@ -0,0 +1,51 @@
+package io.tricefal.core.cgu
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import java.time.Instant
+import java.util.*
+
+
+@ExtendWith(MockitoExtension::class)
+class CguServiceTest {
+
+    @Mock
+    lateinit var repository: ICguAdapter
+
+    lateinit var service: ICguService
+
+    @Test
+    fun `should create a login successfully`() {
+        // Arrange
+        val cgu = CguDomain("kong@gmail.com", Instant.now(), "v1")
+        Mockito.`when`(repository.save(cgu)).thenReturn(cgu)
+        service = CguService(repository)
+
+        // Act
+        val result = service.save(cgu)
+
+        // Arrange
+        Mockito.verify(repository).save(cgu)
+        Assertions.assertEquals(cgu, result)
+    }
+
+    @Test
+    fun `should retrieve last logins by username`() {
+        // Arrange
+        val username = "kong@gmail.com"
+        val cgu = CguDomain("kong@gmail.com", Instant.now(), "v1")
+
+        Mockito.`when`(repository.findByUsername(username)).thenReturn(Optional.of(cgu))
+        service = CguService(repository)
+
+        // Act
+        val result = service.findByUsername(username)
+
+        // Arrange
+        Assertions.assertEquals(cgu, result.get())
+    }
+}
